c7b1a6e047f598ca8efaa927eac2ee5b6b0bb155
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2020-05-03 12:48:45 -0500

    Write dummy implementation.

--- src/ffi/dummy.rs
+++ src/ffi/dummy.rs
@@ -1,198 +1,52 @@
-use crate::*;
+use crate::frame::Frame;
+use std::marker::PhantomData;
+use std::task::Context;
+use std::task::Poll;
 
-use stdweb;
-use stdweb::js;
-
-macro_rules! enclose {
-    ( ($( $x:ident ),*) $y:expr ) => {
-        {
-            $(let $x = $x.clone();)*
-            $y
-        }
-    };
+pub(crate) struct Player<F: Frame> {
+    _phantom: PhantomData<F>,
 }
 
-pub struct Speaker {
-    // sound_device: *mut snd_pcm_t,
-    // buffer_size: u64,
-    // period_size: u64,
-    buffer: Vec<i16>,
-}
+impl<F: Frame> Player<F> {
+    pub(crate) fn new(sr: u32) -> Option<Self> {
+        let _phantom = PhantomData::<F>;
+        let _ = sr;
 
-impl Speaker {
-    pub fn new(sr: u32) -> Result<Speaker, AudioError> {
-        /*let sound_device: *mut snd_pcm_t = pcm_open(false, b"default\0")?;
-        let hw_params = pcm_hw_params(sr, sound_device)?;
-
-        // Get the buffer size.
-        let mut buffer_size = 0;
-        unsafe {
-            snd_pcm_hw_params_get_buffer_size(hw_params, &mut buffer_size);
-        }
-        //        dbg!(buffer_size);
-        let (mut period_size, mut d) = (0, 0);
-        unsafe {
-            snd_pcm_hw_params_get_period_size(
-                hw_params,
-                &mut period_size,
-                &mut d,
-            );
-        }
-        //        dbg!(period_size);
-
-        unsafe {
-            snd_pcm_hw_params_free(hw_params);
-        }
-
-        if unsafe { snd_pcm_prepare(sound_device) } < 0 {
-            panic!(
-                "Could not prepare!".to_string(),
-            );
-        }
-
-        let buffer_size = buffer_size as u64;
-        let period_size = period_size as u64;*/
-
-        let buffer = Vec::new();
-
-        Ok(Speaker {
-            //sound_device,
-            //buffer_size,
-            //period_size,
-            buffer,
-        })
+        None
     }
 
-    pub fn play(&mut self, generator: &mut dyn FnMut() -> (i16, i16)) {
-        /*let _ = unsafe {
-            snd_pcm_status(
-                self.sound_device,
-                self.status.as_mut_ptr() as *mut _,
-            )
-        };
-        let avail = unsafe {
-            snd_pcm_status_get_avail(self.status.as_ptr() as *const _)
-        };
-        let left = self.buffer_size - avail;
+    pub(crate) fn poll(&mut self, cx: &mut Context) -> Poll<()> {
+        let _ = cx;
+    
+        Poll::Pending
+    }
 
-        let buffer_length = self.period_size * 4; // 16 bit (2 bytes) * Stereo (2 channels)
-
-        let write = if left < buffer_length {
-            buffer_length - left
-        } else {
-            0
-        };
-
-        self.buffer.clear();
-
-        for _i in 0..write {
-            let (l, r) = generator();
-            self.buffer.push(l);
-            self.buffer.push(r);
-        }
-
-        if unsafe {
-            snd_pcm_writei(
-                self.sound_device,
-                self.buffer.as_mut_ptr(),
-                write as u64,
-            )
-        } < 0
-        {
-            println!("Buffer underrun");
-        }*/
+    pub(crate) fn play_last(&mut self, audio: &[F]) -> usize {
+        let _ = audio;
+    
+        0 // 0 frames were written.
     }
 }
 
-impl Drop for Speaker {
-    fn drop(&mut self) {
-        /*unsafe {
-            snd_pcm_close(self.sound_device);
-        }*/
-    }
+pub(crate) struct Recorder<F: Frame> {
+    _phantom: PhantomData<F>,
 }
 
-pub struct Microphone {
-    // sound_device: *mut snd_pcm_t,
-    buffer: Vec<i16>,
-}
+impl<F: Frame> Recorder<F> {
+    pub(crate) fn new(sr: u32) -> Option<Self> {
+        let _phantom = PhantomData::<F>;
+        let _ = sr;
 
-impl Microphone {
-    pub fn new(sr: u32) -> Result<Microphone, AudioError> {
-        /*        let sound_device: *mut snd_pcm_t = pcm_open(true, b"default\0")?;
-        let hw_params = pcm_hw_params(sr, sound_device)?;
-
-        // Get the buffer size.
-        let mut buffer_size = 0;
-        unsafe {
-            snd_pcm_hw_params_get_buffer_size(hw_params, &mut buffer_size);
-        }
-        //        dbg!(buffer_size);
-        let (mut period_size, mut d) = (0, 0);
-        unsafe {
-            snd_pcm_hw_params_get_period_size(
-                hw_params,
-                &mut period_size,
-                &mut d,
-            );
-        }
-        //        dbg!(period_size);
-
-        unsafe {
-            snd_pcm_hw_params_free(hw_params);
-        }
-
-        if unsafe { snd_pcm_start(sound_device) } < 0 {
-            panic!(
-                "Could not start!".to_string(),
-            );
-        }*/
-
-        let buffer = Vec::new();
-
-        Ok(Microphone {
-            // sound_device,
-            buffer,
-        })
+        None
     }
 
-    pub fn record(&mut self, generator: &mut dyn FnMut(usize, i16, i16)) {
-        /*let _ = unsafe {
-            snd_pcm_status(
-                self.sound_device,
-                self.status.as_mut_ptr() as *mut _,
-            )
-        };
-        let avail = unsafe {
-            snd_pcm_status_get_avail(self.status.as_ptr() as *const _)
-        };
-
-        self.buffer.resize(avail as usize * 2, 0);
-
-        if unsafe {
-            snd_pcm_readi(
-                self.sound_device,
-                self.buffer.as_mut_ptr(),
-                avail as u64,
-            )
-        } < 0
-        {
-            println!("Buffer overflow.");
-        }
-
-        for i in 0..((avail as usize) / 2) {
-            let l = self.buffer[i * 2 as usize];
-            let r = self.buffer[i * 2 + 1 as usize];
-
-            generator(0, l, r);
-        }*/
+    pub(crate) fn poll(&mut self, cx: &mut Context) -> Poll<()> {
+        let _ = cx;
+    
+        Poll::Pending
     }
-}
 
-impl Drop for Microphone {
-    fn drop(&mut self) {
-        /*unsafe {
-            snd_pcm_close(self.sound_device);
-        }*/
+    pub(crate) fn record_last(&mut self, audio: &mut Vec<F>) {
+        let _ = audio;
     }
 }
