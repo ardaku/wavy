c40f6dcdee579a4dd4407f080548dbe54fb0615b
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2020-05-03 13:04:30 -0500

    MacOS and Windows guaranteed to build with dummy implementation.

--- src/ffi/macos.rs
+++ src/ffi/macos.rs
@@ -1,3 +1,58 @@
+use crate::frame::Frame;
+use std::marker::PhantomData;
+use std::task::Context;
+use std::task::Poll;
+
+pub(crate) struct Player<F: Frame> {
+    _phantom: PhantomData<F>,
+}
+
+impl<F: Frame> Player<F> {
+    pub(crate) fn new(sr: u32) -> Option<Self> {
+        let _phantom = PhantomData::<F>;
+        let _ = sr;
+
+        None
+    }
+
+    pub(crate) fn poll(&mut self, cx: &mut Context) -> Poll<()> {
+        let _ = cx;
+    
+        Poll::Pending
+    }
+
+    pub(crate) fn play_last(&mut self, audio: &[F]) -> usize {
+        let _ = audio;
+    
+        0 // 0 frames were written.
+    }
+}
+
+pub(crate) struct Recorder<F: Frame> {
+    _phantom: PhantomData<F>,
+}
+
+impl<F: Frame> Recorder<F> {
+    pub(crate) fn new(sr: u32) -> Option<Self> {
+        let _phantom = PhantomData::<F>;
+        let _ = sr;
+
+        None
+    }
+
+    pub(crate) fn poll(&mut self, cx: &mut Context) -> Poll<()> {
+        let _ = cx;
+    
+        Poll::Pending
+    }
+
+    pub(crate) fn record_last(&mut self, audio: &mut Vec<F>) {
+        let _ = audio;
+    }
+}
+
+/*
+
 use std::ffi::c_void;
 
 use crate::*;
@@ -321,3 +376,5 @@ impl Drop for Microphone {
         }*/
     }
 }
+
+*/
